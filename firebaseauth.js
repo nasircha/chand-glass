e6d1fe0b85ea093eab90e1828861d6b988127381
Nasir Hussain <mdnasirhussain121@gmail.com>
2025-07-11 10:34:30 +0530

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,12 +1,7 @@
 // firebaseauth.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// ✅ Your Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBXe-82X2gLedCts-GTZNEtt9uklH8DwKg",
   authDomain: "chand-glass-a09d8.firebaseapp.com",
@@ -16,9 +11,7 @@ const firebaseConfig = {
   appId: "1:826931709072:web:a75632d13b7a7c1f934415"
 };
 
-// ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// ✅ Export auth functions
-export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword };
+export { auth };
